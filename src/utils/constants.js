a7f3f35b10284daf054029ab5b84d2d171ead645
Ruben Vicente <2021116429@uac.pt>
2026-01-16 00:50:12 -0100

    Names order

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,19 +1,19 @@
 export const TIMES_L2 = [
   "🇮🇹 AC Milan", "🇳🇱 Ajax", "🇧🇪 Anderlecht", "🇽🇪 Aston Villa", "🇩🇪 Bayern", "🇩🇪 Borussia Dortmund", "🇵🇹 Braga", "🇮🇹 Como", "🇰🇿 FC Astana", "🇵🇹 FC Porto", "🇫🇴 KÍ Klaksvík", "🇽🇪 Manchester City", "🇫🇷 Marseille", "🇫🇷 Monaco", "🇮🇹 Palermo", "🇫🇷 Paris Saint-Germain", "🇵🇹 Rio Ave", "🇪🇸 Sevilla", "🇽🇼 Swansea City", "🇽🇪 West Ham"
-].sort();
+];
 
 export const TIMES_L3 = [
   "🇽🇪 Arsenal", "🇵🇹 Benfica", "🇽🇪 Brighton", "🇪🇸 Celta de Vigo", "🇽🇸 Celtic", "🇽🇪 Chelsea", "🇽🇸 Dundee FC", "🇽🇪 Leicester City", "🇽🇪 Liverpool", "🇷🇺 Lokomotiv Moskva", "🇧🇬 Ludogorets", "🇽🇪 Millwall", "🇮🇹 Parma", "🇫🇷 Pau FC", "🇳🇱 PSV", "🇺🇦 Shakhtar Donetsk", "🇩🇪 Stuttgart", "🇩🇪 Werder Bremen", "🇽🇼 Wrexham", "🇨🇭 Young Boys"
-].sort();
+];
 
 export const TIMES_L4 = [
   "🇪🇸 Barcelona", "🇽🇪 Corinthian-Casuals", "🇵🇹 Fátima", "🇹🇷 Fenerbahçe", "🇮🇹 Fiorentina", "🇮🇹 Internazionale", "🇮🇹 Juventus", "🇦🇲 Pyunik Yerevan", "🇪🇸 Real Bétis", "🇪🇸 Real Madrid", "🇽🇪 Southampton", "🇩🇪 St. Pauli", "🇪🇸 Villarreal", "🇽🇪 Wolverhampton"
-].sort();
+];
 
 
 export const TIMES_EL = [ 
   "🇬🇷 AEK Athens", "🇮🇹 AS Roma", "🇳🇴 Bodø/Glimt", "🇵🇹 Braga", "🇽🇪 Chelsea", "🇮🇹 Chievo Verona", "🇩🇰 Copenhagen", "🇽🇪 Derby County", "🇽🇪 Dundee FC", "🇭🇺 Ferencváros", "🇹🇷 Galatasaray", "🇮🇹 Internazionale", "🇫🇴 KÍ Klaksvík", "🇽🇪 Liverpool", "🇫🇷 Marseille", "🇽🇪 Millwall"
-].sort();
+];
 
 export const RODADAS_EL = [
   "1", "2", "3", "4", "5",
